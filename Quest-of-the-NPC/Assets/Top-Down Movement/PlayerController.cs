a10abe7dd2eac22d3b94b2e39ba7a70f6019901d
Audrey Schmitt <56131819+aaschmitt@users.noreply.github.com>
2022-10-05 22:02:21 -0400

    Player Movement & Camera Follow
    Upon starting the project, user should be able to use "WASD" keys to move the player around the screen. The camera will follow the player.
    
    Imported "Input System" package.

---
+++ Quest-of-the-NPC/Assets/Top-Down Movement/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    
+    private PlayerInputActions playerInput;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveInput = playerInput.Movement.Move.ReadValue<Vector2>();
+        rb.velocity = moveInput * speed;
+    }
+}
