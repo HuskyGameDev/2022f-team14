80698f5bcbb1cb055f708aa951e52a0f52af0299
JamesHenthorn <jameshenthorn2@gmail.com>
2022-10-17 22:12:02 -0400

    Basic health/damage system
    Created a generalized health system that can be referenced for anything that has health.
    Created a gameManager script for holding permanent objects (like the player)
    Set debug damage/heal to key presses and a print to console.

---
+++ Quest-of-the-NPC/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // GameManager object. We make it as an object to create a singleton to ensure that only one exists.
+    public static GameManager gameManager { get; private set; }
+
+    // Creates an EntityHealth object and gives it to the player.
+    // We only have one player, and it always exists. Therefore it is in the GameManager
+    public EntityHealth _playerHealth = new EntityHealth(100, 100, 0);
+ 
+    void Awake()
+    {
+        // If a gameManager object already exists and another is created, get rid of it.
+        // Otherwise, make the gameManager
+        if (gameManager != null && gameManager != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            gameManager = this;
+        }
+    }
+
+   
+}
