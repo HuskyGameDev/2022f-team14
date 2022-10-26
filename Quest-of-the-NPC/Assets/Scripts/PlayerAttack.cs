152f1216de6bc1c87d0ed0bfd6bf4d22513a1e56
cmroth <90865077+cmroth@users.noreply.github.com>
2022-10-26 17:19:50 -0400

    DirectionalMelee
    Melee attack now follows the direction of the player

--- Quest-of-the-NPC/Assets/Scripts/PlayerAttack.cs
+++ Quest-of-the-NPC/Assets/Scripts/PlayerAttack.cs
@@ -12,16 +12,22 @@ public class PlayerAttack : MonoBehaviour
 
     [SerializeField] public float attackArea;
     [SerializeField] public int damage;
+    public int face;
+    
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        directionalOffset(1);
+        directionalOffset(0);
         //if sufficient time has passed
         if (attackCoolDown <= 0)
         {
+            
             //melee button press
             if (Input.GetKey(KeyCode.Space))
             {
+                
                 //apply damage to all enemies or breakable objects in range
                 Collider2D[] enemies = Physics2D.OverlapCircleAll(attackDirection.position, attackArea, canBreak);
                 for (int i = 0; i < enemies.Length; i++)
@@ -30,6 +36,7 @@ public class PlayerAttack : MonoBehaviour
                 }
                 //reset timer
                 attackCoolDown = attackTime;
+                
             }
         }
         else 
@@ -37,5 +44,42 @@ public class PlayerAttack : MonoBehaviour
             //adjust timer
             attackCoolDown -= Time.deltaTime;
         }
+        
+    }
+
+    //adjust poition of melee sphere according to player direction
+    void directionalOffset(int time) 
+    {
+        //where is the player currently facing
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            attackDirection.position += new Vector3(0,1,0);
+        }
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            attackDirection.position += new Vector3(1,0,0);
+        }
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            attackDirection.position += new Vector3(0,-1,0);
+        }
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            attackDirection.position += new Vector3(-1,0,0);
+        }
+
+        //reset attack area to match player position
+        if (time == 1) 
+        {
+            attackDirection.position = transform.position;
+            
+        }
+    }
+
+    //Shows melee area in scene view
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackDirection.position, attackArea);
     }
 }
